d66cf5cc25aeafbe39a7a00f39dad2c9264ff7a3
davi <radaa@gmail.com>
2023-01-19 21:37:41 -0300

    atualizando as rotas do get e getById

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ func main() {
 
 	router := mux.NewRouter()
 	router.HandleFunc("/users", metado.Post).Methods(http.MethodPost)
+	router.HandleFunc("/users", metado.Get).Methods(http.MethodGet)
+	router.HandleFunc("/users/{Id}", metado.GetById).Methods(http.MethodGet)
 
 	fmt.Println("ok")
 	log.Fatal(http.ListenAndServe(":5000", router))
