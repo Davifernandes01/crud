2737a508ac9ccbbfaaa82dfc652714cdaf96ef5c
davi <radaa@gmail.com>
2023-01-22 21:56:04 -0300

    adicionado os metados put e delete

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ func main() {
 	router.HandleFunc("/users", metado.Post).Methods(http.MethodPost)
 	router.HandleFunc("/users", metado.Get).Methods(http.MethodGet)
 	router.HandleFunc("/users/{Id}", metado.GetById).Methods(http.MethodGet)
+	router.HandleFunc("/users/{Id}", metado.Update).Methods(http.MethodPut)
+	router.HandleFunc("/users/{Id}", metado.Delete).Methods(http.MethodDelete)
 
 	fmt.Println("ok")
 	log.Fatal(http.ListenAndServe(":5000", router))
--- metadosHttp/metados.go
+++ metadosHttp/metados.go
@@ -13,15 +13,15 @@ import (
 
 // struct para a criação do usuario no banco de dados
 type Users struct {
-	Id    uint32 `json:"id"`
+	Id    int32  `json:"id"`
 	Nome  string `json:"nome"`
 	Email string `json:"email"`
 }
 
 // inserir
 
-//lendo a requisição
-//adicionar
+// lendo a requisição
+// adicionar
 func Post(w http.ResponseWriter, r *http.Request) {
 	request, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -80,8 +80,8 @@ func Post(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(fmt.Sprintf("users inserido com sucesso!! id: %d", id)))
 }
 
-//buscar
-func Get(w http.ResponseWriter, r *http.Request){
+// buscar
+func Get(w http.ResponseWriter, r *http.Request) {
 
 	//abrindo a conexeao
 	db, err := banco.Con()
@@ -94,7 +94,7 @@ func Get(w http.ResponseWriter, r *http.Request){
 	defer db.Close()
 
 	//exexutando uma query que devolve as linhas do banmco de dados
-	rows, err:= db.Query("select * from users")
+	rows, err := db.Query("select * from users")
 	if err != nil {
 		w.Write([]byte("erro ao buscar users"))
 
@@ -107,20 +107,19 @@ func Get(w http.ResponseWriter, r *http.Request){
 	//criando um slice para armazenar os users
 	var users []Users
 
-	for rows.Next(){ //"proxima linha"
-	//para cada linha no banco, ira se criar um novo users
+	for rows.Next() { //"proxima linha"
+		//para cada linha no banco, ira se criar um novo users
 		var user Users
 
-		if err := rows.Scan(&user.Id, &user.Nome, &user.Email);err != nil {
+		if err := rows.Scan(&user.Id, &user.Nome, &user.Email); err != nil {
 			w.Write([]byte("erro ao escanear o users"))
 
 			return
-	    }
+		}
 
 		//assim que se criar um user, ira coloca-lo dentro do slice
 		users = append(users, user)
 	}
-
 
 	//status 200
 	w.WriteHeader(http.StatusOK)
@@ -134,11 +133,10 @@ func Get(w http.ResponseWriter, r *http.Request){
 
 }
 
+// buscar por id
+func GetById(w http.ResponseWriter, r *http.Request) {
 
-//buscar por id
-func GetById(w http.ResponseWriter, r *http.Request){
-
-	parametro:= mux.Vars(r)
+	parametro := mux.Vars(r)
 
 	// id, err:= strconv.ParseUint(parametro["id"],10,32)
 	// if err != nil{
@@ -147,13 +145,13 @@ func GetById(w http.ResponseWriter, r *http.Request){
 	// 	return
 	// }
 
-	ID, erro := strconv.ParseUint(parametro["id"], 10, 32)
+	ID, erro := strconv.ParseInt(parametro["Id"], 10, 32)
 	if erro != nil {
 		w.Write([]byte("Erro ao converter o parâmetro para inteiro"))
 		return
 	}
 
-	db, err:= banco.Con()
+	db, err := banco.Con()
 	if err != nil {
 		w.Write([]byte("erro ao se conecetar com o banco"))
 
@@ -162,27 +160,114 @@ func GetById(w http.ResponseWriter, r *http.Request){
 
 	defer db.Close()
 
-
-	row, err:= db.Query("select * from users where id = ?", ID)
-	if err != nil{
+	row, err := db.Query("select * from users where id = ?", ID)
+	if err != nil {
 		w.Write([]byte("erro ao buscar usuario"))
 		return
 	}
 
 	var user Users
 
-	if row.Next(){
+	if row.Next() {
 
-		if err:= row.Scan(&user.Id, &user.Nome, &user.Email); err != nil{
+		if err := row.Scan(&user.Id, &user.Nome, &user.Email); err != nil {
 			w.Write([]byte("erro ao escanear usuario"))
 			return
 		}
 	}
 
-	if err := json.NewEncoder(w).Encode(user); err !=nil{
+	if err := json.NewEncoder(w).Encode(user); err != nil {
 
 		w.Write([]byte("erro ao converter o usuario para json"))
 		return
 	}
 
+}
+
+// atualiza os dados do usuario
+func Update(w http.ResponseWriter, r *http.Request) {
+
+	parametros:= mux.Vars(r)
+
+	Id, err:= strconv.ParseInt(parametros["Id"], 10, 32)
+
+	if err !=nil {
+		w.Write([]byte("erro ao converter parametro para inteiro"))
+
+		return
+	}
+
+	body, err:= ioutil.ReadAll(r.Body)
+	if err !=nil {
+		w.Write([]byte("falha ao ler o corpo da requisicao"))
+		return
+	}
+
+	var user Users
+
+	if err:= json.Unmarshal(body, &user); err != nil{
+		w.Write([]byte("erro ao converter json para struct"))
+
+		return
+	}
+
+	db, err:= banco.Con()
+	if err != nil{
+		w.Write([]byte("erro ao se conectar com o banco de dados"))
+
+		return
+	}
+
+	defer db.Close()
+
+	sts, err:=db.Prepare("update users set nome = ?, email = ? where id = ?") 
+	if err != nil {
+		w.Write([]byte("erro ao criar o sts"))
+		return
+	}
+
+	defer sts.Close()
+
+	if _, err := sts.Exec(user.Nome, user.Email, Id); err != nil {
+
+		w.Write([]byte("erro ao atualizar o usuario"))
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+//deleta os users no banco
+func Delete(w http.ResponseWriter, r *http.Request){
+
+	parametro:= mux.Vars(r)
+
+	Id, err:= strconv.ParseInt(parametro["Id"], 10, 32)
+	if err!=nil{
+		w.Write([]byte("erro ao converter parametro para inteiro"))
+		return
+	}
+
+	db,err:= banco.Con()
+	if err !=nil {
+		w.Write([]byte("erro ao se conectar com o banco de dados"))
+		return
+	}
+
+	defer db.Close()
+
+	sts,err:= db.Prepare("delete from users where id = ? ")
+	if err != nil {
+		w.Write([]byte("erro ao executar o statement"))
+
+		return
+	}
+
+	defer sts.Close()
+
+	if _, err:= sts.Exec(Id); err != nil {
+		w.Write([]byte("erro ao deletar usuario"))
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
